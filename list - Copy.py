f734399306d65b2c533599bef24b63119118ab93
Ranjana KB <ranjanabalakrishnan555@gmail.com>
2025-04-01 20:55:51 +0530

    Add files via upload

---
+++ list - Copy.py
@@ -0,0 +1,8 @@
+# Write a Python program to compute the mean, median, and mode of a list of number.
+from statistics import mean, median, mode
+
+numbers = [1, 2, 3, 3, 4, 5, 5, 5]
+
+print("Mean:", mean(numbers))
+print("Median:", median(numbers))
+print("Mode:", mode(numbers))
